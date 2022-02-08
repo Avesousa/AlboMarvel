5c5361d298ec41ff07654b06c5fa42af3897b59e
Avelino Figueira <avesousa14@gmail.com>
2022-02-08 10:03:25 -0300

    Add file with main character

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO HERO (ID, NAME, USERNAME) VALUES (1010913, 'Captain America (Ultimate)', 'capamerica');
+INSERT INTO HERO (ID, NAME, USERNAME) VALUES (1010935, 'Iron Man (Ultimate)', 'ironman');

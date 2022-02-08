75760a6d0d13c424d6b576a8ed2620bc204165d6
Avelino Figueira <avesousa14@gmail.com>
2022-02-08 09:50:54 -0300

    Changes for converter

---
+++ src/main/java/com/albo/marvel/exception/NotFoundContentException.java
@@ -0,0 +1,16 @@
+package com.albo.marvel.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NotFoundContentException extends Exception{
+    
+    private String message;
+    
+    public NotFoundContentException(String message) {
+        this.message = message;
+    }
+    
+}

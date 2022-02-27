60c9a5264c71fac455577149a42109453beb2a0e
Avelino Figueira <avesousa14@gmail.com>
2022-02-26 23:19:43 -0300

    Add test

---
+++ src/test/java/com/albo/marvel/MarvelApplicationTest.java
@@ -0,0 +1,12 @@
+package com.albo.marvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MarvelApplicationTest {
+
+    @Test
+    void contextLoad(){}
+    
+}
